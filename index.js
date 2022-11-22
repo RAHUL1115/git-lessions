ea625da7b44a7e9784307a4d362b5a737f7b656b
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-11-22 16:33:59 +0530

    added improvement and performence boost

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-require('dotenv').config();
-const fs = require('fs');
-const pgClient = require('pg').Client
-const Cursor = require('pg-cursor')
-const csv = require('fast-csv');
+require("dotenv").config();
+const fs = require("fs");
+const pgClient = require("pg").Client;
+const Cursor = require("pg-cursor");
+const csv = require("fast-csv");
 
+const config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
 
-let client = new pgClient({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const client = new pgClient(config);
 
+(async () => {
+  try {
+    await client.connect();
+    let batchSize = 100
 
-client.connect().then(()=> {
-    const query = {
-        name: 'get-name',
-        text: 'SELECT * FROM "Million"',
-        rowMode: String,
-    }
+    let query = 'SELECT * FROM "Million"';
+    const cursor = client.query(new Cursor(query));
     
-    const cursor = client.query(new Cursor(query.text));
+    // csvStream
+    const csvStream = csv.format({ headers: true }).transform((row) => ({
+      name: row.name,
+      joindate: row.joindate,
+      currentdate: new Date(),
+    }));
 
-    // 1000000
-    cursor.read(50000, (err,rows)=>{
-        var writeStream = fs.createWriteStream('./output.csv');
-        const csvStream = csv.format({ headers: true }).transform(row => ({
-            name : row.name,
-            joindate : row.joindate,
-            currentdate: new Date(),
-        }));
+    // create fs writable stream
+    var writeStream = fs.createWriteStream("./output.csv");
+    csvStream.pipe(writeStream);
+    
+    // streaming data through csv stream
+    let rows = await cursor.read(batchSize);
+    rows.forEach((row) => {
+      csvStream.write(row);
+    });
+    while(rows.length){
+      rows = await cursor.read(batchSize);
+      rows.forEach((row) => {
+        csvStream.write(row);
+      });
+    }
 
-        csvStream.pipe(writeStream)
-
-        rows.forEach(row=>{
-            csvStream.write(row);
-        })
-
-        csvStream.end();
-        cursor.close()
-        client.end()
-    })
-});
+    // close all connection
+    csvStream.end();
+    cursor.close();
+    client.end();
+  } catch (error) {
+    console.error(error);
+  }
+})();
