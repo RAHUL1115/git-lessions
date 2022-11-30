715f085bf7070c963a2fa2bda9aab7ae652d6173
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-11-30 12:47:11 +0530

    version 3.0

--- index.js
+++ index.js
@@ -1,68 +1,137 @@
-require("dotenv").config();
+// require("dotenv").config();
 const fs = require("fs");
 const pgClient = require("pg").Client;
-const Cursor = require("pg-cursor");
+const QueryStream = require("pg-query-stream");
 const csv = require("fast-csv");
 
 const config = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || "5432",
+  user: process.env.DB_USER || "pguser",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "test",
 };
 
 const client = new pgClient(config);
 
+function withTransaction(client,filename) {
+  return new Promise(async (resolve, reject) => {
+    await client.query("BEGIN");
+    try {
+      const writeStream = fs.createWriteStream(filename);
+      const csvStream = csv
+        .format({ headers: true })
+        .transform((row) => ({
+          name: row.name,
+          joindate: row.joindate.toJSON(),
+          current_ts: row.current_ts.toJSON(),
+          statement_ts: row.statement_ts.toJSON(),
+        }))
+        .on("end", () => {
+          writeStream.end();
+          resolve();
+        });
+
+      csvStream.pipe(writeStream);
+
+      for (let i = 0; i < 5; i++) {
+        const response = await client.query(
+          `SELECT *, current_timestamp AS current_ts, statement_timestamp() AS statement_ts FROM "Million" OFFSET ${
+            100 * i
+          } LIMIT 100;`
+        );
+        response.rows.forEach((row) => {
+          csvStream.write(row);
+        });
+      }
+      await client.query("COMMIT");
+      csvStream.end();
+    } catch (err) {
+      await client.query("ROLLBACK");
+    }
+  });
+}
+
+function withoutTransaction(client,filename) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const writeStream = fs.createWriteStream(filename);
+      const csvStream = csv
+        .format({ headers: true })
+        .transform((row) => ({
+          name: row.name,
+          joindate: row.joindate.toJSON(),
+          current_ts: row.current_ts.toJSON(),
+          statement_ts: row.statement_ts.toJSON(),
+        }))
+        .on("end", () => {
+          writeStream.end();
+          resolve();
+        });
+
+      csvStream.pipe(writeStream);
+
+      for (let i = 0; i < 5; i++) {
+        const response = await client.query(
+          `SELECT *, current_timestamp AS current_ts, statement_timestamp() AS statement_ts FROM "Million" OFFSET ${
+            100 * i
+          } LIMIT 100;`
+        );
+        response.rows.forEach((row) => {
+          csvStream.write(row);
+        });
+      }
+      csvStream.end();
+    } catch (err) {}
+  });
+}
+
+function withStream(client,filename) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const writeStream = fs.createWriteStream(filename);
+
+      let selectQuery = `SELECT *, current_timestamp AS current_ts, statement_timestamp() AS statement_ts FROM "Million" LIMIT 500;`;
+      const stream = client.query(new QueryStream(selectQuery));
+
+      stream
+        .pipe(csv.format({ headers: true }))
+        .transform((row, next) => {
+          return next(null, {
+            name: row.name,
+            joindate: row.joindate.toJSON(),
+            current_ts: row.current_ts.toJSON(),
+            statement_ts: row.statement_ts.toJSON(),
+          });
+        })
+        .pipe(writeStream);
+
+      stream.on("end", () => {
+        writeStream.end();
+        resolve();
+      });
+    } catch (err) {}
+  });
+}
+
 (async () => {
   try {
     await client.connect();
-    let writeStream = fs.createWriteStream("./output.csv");
-    let batchSize = 100
-    
-    let createQuery = `CREATE TABLE IF NOT EXISTS "Million" (name character varying(255) COLLATE pg_catalog."default", joindate date );`
 
+    let createQuery = `CREATE TABLE IF NOT EXISTS "Million" (name character varying(255) COLLATE pg_catalog."default", joindate date );`;
     await client.query(createQuery);
 
-    let truncateQuery = `TRUNCATE "Million";`
+    let truncateQuery = `TRUNCATE "Million";`;
+    await client.query(truncateQuery);
 
-    await client.query(createQuery);
-
-    let insertQuery = `INSERT INTO "Million" (name, joindate) SELECT substr(md5(random()::text), 1, 10), DATE '2018-01-01' + (random() * 700)::integer FROM generate_series(1, 100000);`
-
+    let insertQuery = `INSERT INTO "Million" (name, joindate) SELECT substr(md5(random()::text), 1, 10), DATE '2018-01-01' + (random() * 700)::integer FROM generate_series(1, 100000);`;
     await client.query(insertQuery);
 
-    // let query = `SELECT *, statement_timestamp() AS timestamp FROM "Million";`;
-    let selectQuery = `SELECT *, current_timestamp AS timestamp FROM "Million";`;
+    await withTransaction(client,'./output/withTransaction.csv');
+    await withoutTransaction(client,'./output/withoutTransaction.csv');
+    await withStream(client,'./output/stream.csv');
 
-
-    const cursor = client.query(new Cursor(selectQuery));
-    
-    // csvStream
-    const csvStream = csv.format({ headers: true }).transform((row) => ({
-      name: row.name,
-      joindate: row.joindate.toJSON(),
-      timestamp : row.timestamp.toJSON(),
-      currentdate: (new Date()).toJSON(),
-    }));
-
-    // create fs writable stream
-    csvStream.pipe(writeStream);
-    
-    // streaming data through csv stream
-    let rows = [null]
-    while(rows.length){
-      rows = await cursor.read(batchSize);
-      rows.forEach((row) => {
-        csvStream.write(row);
-      });
-    }
-
-    // close all connection
-    csvStream.end();
-    cursor.close();
     client.end();
-    console.log("done");
   } catch (error) {
     console.error(error);
   }
